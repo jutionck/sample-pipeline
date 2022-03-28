81d356ae3a326c536badf088087bcbf890c9d647
Jution Candra Kirana <jutionck@gmail.com>
2022-03-28 16:15:05 +0700

    Create a Dockerfile

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Print("Hello World")
+
+	route := gin.Default()
+	route.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := route.Run(":8081")
+	if err != nil {
+		return
+	}
 }
